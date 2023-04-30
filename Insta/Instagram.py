41153deae07668b80d06cdd285f07ae23a309191
PoMalaW <pomalow.gaming@gmail.com>
2023-04-30 18:21:44 +0200

    cleaning commit

--- Insta/Instagram.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.instagram.com/pomalaw/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-soup.prettify()
-print(soup)
--- main.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-client = discord.Bot(intents=discord.Intents.all())
-
-@client.event
-async def on_ready():
-    print("[INFO] Bot is ready !")
-
-client.run(open("token.txt", "r").readline())
